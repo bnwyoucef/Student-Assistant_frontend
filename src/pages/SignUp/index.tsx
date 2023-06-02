98f0e30969ed8a3e8b15694f578c3fea473ec9cb
Youcef Bounoua <youcef@hotello.com>
2023-06-02 15:32:20 +0100

    add sign-up logic

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,35 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 import "./index.css"
+import axios from '../../Api/Axios';
 
 const SignUp = () => {
-    const [info,setInfo] = useState({});
-    const [password, setpassword] = useState('');
-    function signIn() {
-      return false;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  async function signUp(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (password === confirmPassword) {
+      try {
+        const response = await axios.post("users/sign-up", { email, password });
+        console.log(response.data);
+        if (response.data.status === 409) {
+          setErrorMessage(response.data.message)
+        }
+      } catch (error: any) {
+        console.log(error.response?.data?.message[0]);
+        setErrorMessage(error.response?.data?.message[0]);
+      }
+    } else {
+      setErrorMessage("Passwords do not match!");
     }
+  }
+
+  useEffect(() => {
+    setErrorMessage('');
+  }, [email, password, confirmPassword])
+
   return (
     <div className="min-height-form flex flex-col justify-center">
       <div
@@ -20,7 +43,7 @@ const SignUp = () => {
           />
         </div>
         <div className="w-full xl:w-1/2 p-8">
-          <form onSubmit={signIn}>
+          <form onSubmit={signUp}>
             <h1 className=" text-2xl font-bold">Create an account!</h1>
             <div>
               <span className="text-gray-600 text-sm">
@@ -39,9 +62,12 @@ const SignUp = () => {
               </label>
               <input
                 className="text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline h-10"
+                required
                 id="email"
                 type="text"
                 placeholder="Your email address"
+                value={email}
+                onChange={event => setEmail(event.target.value)}
               />
             </div>
             <div className="mb-4 mt-6">
@@ -53,9 +79,12 @@ const SignUp = () => {
               </label>
               <input
                 className="text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline h-10"
+                required
                 id="password"
                 type="password"
                 placeholder="Your password"
+                value={password}
+                onChange={event => setPassword(event.target.value)}
               />
             </div>
             <div className="mb-6 mt-6">
@@ -67,15 +96,25 @@ const SignUp = () => {
               </label>
               <input
                 className="text-sm bg-gray-200 appearance-none rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline h-10"
+                required
                 id="confirmPassword"
                 type="password"
                 placeholder="Confirm your password"
+                value={confirmPassword}
+                onChange={event => setConfirmPassword(event.target.value)}
               />
+            </div>
+            <div className="mb-6 mt-6">
+              {errorMessage &&
+                <span
+                  className="text-md appearance-none rounded w-full py-2 px-3 text-red-500 mb-1 leading-tight h-10"
+                  id="errorMsg"
+                >{errorMessage}</span>}
             </div>
             <div className="flex w-full mt-8">
               <button
                 className="w-full bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
-                type="button"
+                type="submit"
               >
                 Sign up
               </button>
