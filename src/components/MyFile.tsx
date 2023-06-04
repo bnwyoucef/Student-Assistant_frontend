d94c5cc201247f5f67b07de3349558caae0f4589
Youcef Bounoua <youcef@hotello.com>
2023-06-04 23:43:14 +0100

    add files list

---
+++ src/components/MyFile.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const MyFile = () => {
+  return (
+    <tr className="text-sm leading-none text-gray-600 h-16 cursor-pointer">
+        <td className="w-1/2">
+            <div className="flex items-center">
+                <div className="w-10 h-10 bg-red-500 rounded-sm flex items-center justify-center">
+                    <p className="text-xs font-bold leading-3 text-white">PDF</p>
+                </div>
+                <div className="pl-2">
+                    <p className="text-sm font-medium leading-none text-gray-800">file_name.pdf</p>
+                </div>
+            </div>
+        </td>
+        <td>
+            <button class="bg-grey-light text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center hover:bg-yellow-200 ">
+                <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
+                </svg>
+                <span>Download</span>
+            </button>
+        </td>
+        <td>
+            <button class="bg-grey-light text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center hover:bg-red-500 hover:text-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+            </svg>
+                    <span>remove</span>
+            </button>
+        </td>
+    </tr>
+  )
+}
+
+export default MyFile
--- src/pages/Files/index.tsx
+++ src/pages/Files/index.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import MyFile from './../../components/MyFile';
 
 const Files = () => {
+  const [files, setFiles] = useState([1,2,3,4,5,6])
+  useEffect(() => {
+    return async () => {
+      try {
+        //fetch the files and set it to the files array
+      } catch (error:any) {
+        //setErrMsg(error.message);
+      }
+    }
+  }, [])
+
+  const fileList = files.map((myFile, index) => {
+    return <MyFile key={index} />
+  });
+
   return (
-    <div>this is Files page</div>
+    <>
+      <div className="xl:w-3/4 2xl:w-4/5 w-full">
+          <div className="px-4 md:px-10 py-4 md:py-7">
+              <div className="sm:flex items-center justify-between">
+                  <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">Files</p>
+                  <div className="mt-4 sm:mt-0">
+                      <button className="inline-flex sm:ml-3 items-start justify-start px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded">
+                          <p className="text-sm font-medium leading-none text-white">Download All</p>
+                      </button>
+                  </div>
+              </div>
+          </div>
+          <div className="bg-white px-4 md:px-10 pb-5">
+              <div className="overflow-x-auto">
+                  <table className="w-full whitespace-nowrap">
+                      <tbody>
+                        {fileList}
+                      </tbody>
+                  </table>
+              </div>
+          </div>
+      </div>
+    </>
   )
 }
 
