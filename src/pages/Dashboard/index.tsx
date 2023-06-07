4db4457ac17f37ce2e33ed80df6d56ebabc46467
Youcef Bounoua <youcef@hotello.com>
2023-06-07 17:29:39 +0100

    cr

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -74,7 +74,7 @@ const Index = () => {
               </ul>
             </div>
           </div>
-          <div className="p-4">
+          <div className="p-4 cursor-pointer">
             <button onClick={logout} type="button" className="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
               <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="" viewBox="0 0 16 16">
                 <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
